a7c33918f7d5fff9d4cb88f6b17c5c43461c34f0
Matthias <matthiasleroux@laposte.net>
2020-11-08 17:33:25 +0100

    refactor(controller): import Twig Error and rename $var

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\CommentsManager;
 use App\PostsManager;
 use Lib\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 /**
  * Class BlogController
@@ -15,25 +19,33 @@ class BlogController extends AbstractController
     /**
      * @return Response
      *
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function posts(): Response
     {
         $manager = new PostsManager();
-        $postsList = $manager->getList($begin=0, $end=4);
+        $postsList = $manager->getList(0,10);
 
         return $this->render("posts.html.twig",[
             "posts_list" => $postsList
         ]);
     }
 
+    /**
+     * @return Response
+     *
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
     public function post(): Response
     {
-        $manager = new PostsManager();
-        $singlePost = $manager->getSinglePost($_GET['id']);
-        $comments = $manager->getCommentsPost($singlePost[0]['id'], 1);
+        $postsManager = new PostsManager();
+        $singlePost = $postsManager->getSinglePost($_GET['id']);
+        $commentsManager = new CommentsManager();
+        $comments = $commentsManager->getCommentsPost($singlePost[0]['id'], 1);
 
         return $this->render("post.html.twig",[
             "single_post" => $singlePost,
