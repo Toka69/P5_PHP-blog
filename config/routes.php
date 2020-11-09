edafbd64b7b48f2a25b8ac74f2c1eabcac7c26a3
Matthias <matthiasleroux@laposte.net>
2020-11-09 20:53:52 +0100

    feature(router): add backoffice routes

--- config/routes.php
+++ config/routes.php
@@ -2,12 +2,21 @@
 
 use App\Controller\{
     HomeController,
-    BlogController
+    BlogController,
+    BackofficeController
 };
 use Lib\Router\Route;
 
 return [
     new Route("home", "/", HomeController::class, "home"),
     new Route("post", "/posts", BlogController::class, "posts"),
-    new Route("post", "/post", BlogController::class, "post")
+    new Route("post", "/post", BlogController::class, "post"),
+    new Route("admin", "/back", BackofficeController::class, "backoffice"),
+    new Route("admin", "/back-users", BackofficeController::class, "backofficeUsers"),
+    new Route("admin", "/back-posts", BackofficeController::class, "backofficePosts"),
+    new Route("admin", "/back-comments", BackofficeController::class, "backofficeComments"),
+    new Route("admin", "/back-settings", BackofficeController::class, "backofficeSettings"),
+    new Route("admin", "/login", BackofficeController::class, "login"),
+    new Route("admin", "/register", BackofficeController::class, "register"),
+    new Route("admin", "/forgot-password", BackofficeController::class, "forgotPassword")
 ];
