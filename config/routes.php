4f656c5fadec507e20b01c0b45c49686510db8bd
Matthias <matthiasleroux@laposte.net>
2020-11-15 12:05:21 +0100

    refactor(router): A different name for each route

--- config/routes.php
+++ config/routes.php
@@ -3,20 +3,21 @@
 use App\Controller\{
     HomeController,
     BlogController,
-    BackofficeController
+    BackofficeController,
+    SecurityController
 };
 use Lib\Router\Route;
 
 return [
     new Route("home", "/", HomeController::class, "home"),
-    new Route("post", "/posts", BlogController::class, "posts"),
+    new Route("posts", "/posts", BlogController::class, "posts"),
     new Route("post", "/post", BlogController::class, "post"),
-    new Route("admin", "/back", BackofficeController::class, "backoffice"),
-    new Route("admin", "/back-users", BackofficeController::class, "backofficeUsers"),
-    new Route("admin", "/back-posts", BackofficeController::class, "backofficePosts"),
-    new Route("admin", "/back-comments", BackofficeController::class, "backofficeComments"),
-    new Route("admin", "/back-settings", BackofficeController::class, "backofficeSettings"),
-    new Route("admin", "/login", BackofficeController::class, "login"),
-    new Route("admin", "/register", BackofficeController::class, "register"),
-    new Route("admin", "/forgot-password", BackofficeController::class, "forgotPassword")
+    new Route("backoffice", "/back", BackofficeController::class, "backoffice"),
+    new Route("backofficeUsers", "/back-users", BackofficeController::class, "backofficeUsers"),
+    new Route("backofficePosts", "/back-posts", BackofficeController::class, "backofficePosts"),
+    new Route("backofficeComments", "/back-comments", BackofficeController::class, "backofficeComments"),
+    new Route("backofficeSettings", "/back-settings", BackofficeController::class, "backofficeSettings"),
+    new Route("login", "/login", SecurityController::class, "login"),
+    new Route("register", "/register", SecurityController::class, "register"),
+    new Route("forgotPassword", "/forgot-password", SecurityController::class, "forgotPassword")
 ];
