eab49a5cf85d8650c1aac292e81b46a6e69e41dd
Matthias <matthiasleroux@laposte.net>
2020-11-20 21:57:22 +0100

    refactor(security): add session_set_cookie_params

--- public/index.php
+++ public/index.php
@@ -8,6 +8,14 @@ use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\ErrorHandler\ErrorHandler;
 use Symfony\Component\ErrorHandler\DebugClassLoader;
 
+session_set_cookie_params([
+    'lifetime' => 3600,
+    'path' => '/',
+    'domain' => $_SERVER['HTTP_HOST'],
+    'secure' => false,
+    'httponly' => true,
+    'samesite' => 'strict'
+]);
 session_start();
 
 $dotenv = new Dotenv();
