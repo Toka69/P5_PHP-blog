7b85169adc6edf49f4ed017ec05744ef712536a3
Matthias <matthiasleroux@laposte.net>
2020-11-23 22:48:15 +0100

    update blog.sql

--- SQL_demo/blog.sql
+++ SQL_demo/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : ven. 20 nov. 2020 à 20:21
+-- Généré le : lun. 23 nov. 2020 à 21:36
 -- Version du serveur :  10.5.6-MariaDB-1:10.5.6+maria~focal
 -- Version de PHP : 7.4.11
 
@@ -125,12 +125,25 @@ INSERT INTO `users` (`id`, `admin`, `first_name`, `last_name`, `phone`, `email`,
 (4, 0, 'John', 'DOE', '0632548562', 'john.doe@gmail.com', 'test', 'rue des acacias', '12', 75000, 'PARIS', ' ', 'test', 1),
 (5, 0, 'Jean', 'dupont', NULL, 'cendar78@msn.com', 'test', NULL, NULL, NULL, NULL, NULL, NULL, 1),
 (6, 0, 'Louis', 'LEROUX', NULL, 'louis.leroux@laboitemail.fr', 'louis2011', NULL, NULL, NULL, NULL, NULL, NULL, 2),
-(7, 0, 'Marty', 'McFly', NULL, 'marty@futur.com', '$2y$12$E0fWSrgMtDCbcDxwhMvdXuLI7/lIUeUpZWI9UxdidYl/6cP10hzQS', NULL, NULL, NULL, NULL, NULL, NULL, 2),
+(7, 1, 'Marty', 'McFly', NULL, 'marty@futur.com', '$2y$12$E0fWSrgMtDCbcDxwhMvdXuLI7/lIUeUpZWI9UxdidYl/6cP10hzQS', NULL, NULL, NULL, NULL, NULL, NULL, 2),
 (8, 0, 'Jean', 'Rosé', NULL, 'jean.rose@mail.fr', '$2y$12$bOS.z93o7HC37o3XQvWG1.O.qpitGZ23IbF9pJZCKbH1dUUd6wDkO', NULL, NULL, NULL, NULL, NULL, NULL, 3),
 (9, 0, 'Mad', 'Max', '', 'mad@max.fr', '$2y$12$SkAhiI8SnbQ7rpFb8seK6unbICxndabcu68iuge7SM2l7M6d.BWOO', '', '', NULL, '', '', '', 1),
 (10, 0, 'Robo', 'Cop', NULL, 'robocop@corp.fr', '$2y$12$j2acxjRVTygope5qQO7hUeHSce4DcCpfQDV0F7qUi8Rs7hb3zVLzm', NULL, NULL, NULL, NULL, NULL, NULL, 4),
 (11, 0, 'Bat', 'Man', NULL, 'batman@gotham.com', '$2y$12$K8TqgV8xqTbsSIif/mGOc.nnyBsKzn2gtuFnSXyWQA9z.dHtGPzKy', NULL, NULL, NULL, NULL, NULL, NULL, 4),
-(12, 0, 'to', 'to', NULL, 'toto@gmail.from', '$2y$12$EBvGP1B0ADCprT3VqIDmB.uW6Le7hpDdLyeraYsez1XeW8hkPkkru', NULL, NULL, NULL, NULL, NULL, NULL, 4);
+(12, 0, 'to', 'to', NULL, 'toto@gmail.from', '$2y$12$EBvGP1B0ADCprT3VqIDmB.uW6Le7hpDdLyeraYsez1XeW8hkPkkru', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(13, 0, 'François', 'Mitterand', NULL, 'francois@social.fr', '$2y$12$hqZxSvr1raFgPCqCkqos1OwW4zhCrqG3dpwyY.4BY0dfQou5Qu4Z6', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(14, 1, 'hacker', 'jack', NULL, 'jack@lepirate.fr', '$2y$12$2V7KjrEFpLbu.WBSV5bIbeg9t0pFD6T6YQnCwGxJFK3.WG48IVD06', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(15, 0, 'elon', 'musk', NULL, 'elon@tesla.fr', '$2y$12$oTHeY//mlrkuDT6jA.Pe2O2utycnQhWbNCaAJJCQ0oH8CqF1Xj81S', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(16, 0, 'donald', 'trump', NULL, 'donald@disney.fr', '$2y$12$jm/QmOjIqN/BBwAU50ps1.S.5y7qmMSQ9wXSZ5ZUfSwCT8Os30eli', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(17, 0, 'Iron', 'man', NULL, 'iron@marvel.com', '$2y$12$mPSLqxHiulinYRp87xzJe.M.EXKRvU0J0pnphqqEN1NQfjS8CDAey', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(18, 0, 'Jean', 'jack', NULL, 'jeanjack@laposte.net', '$2y$12$gnH79yCfgs7JC7KlhXPQ5..EWiSRsGuohIKlgmljC5RuW3pwu3Lfm', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(19, 0, 'hacker', 'jack', NULL, 'ab@ddd.fr', '$2y$12$rzikly7Erw4OjhGTao1pUOf4F.xtNAmxHX5.P4QHNUJUQCKaIR17q', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(20, 0, 'Matthias', 'jack', NULL, 'dqsdqd@qdssqd.com', '$2y$12$C/.vRfP6nUHmVvlPFhDBpuk9GSiqaECO7siNNo4NE5mMvZeeTn50G', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(21, 0, 'Matthias', 'dupont', NULL, 'sqsqs@qsqsq.fr', '$2y$12$7DdGkdRIVBqB4/S2vU9aS.YNCIYIQwLqpnfZsKOXf16AUWy8UBgCC', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(22, 0, 'qd', 'qdqsd', NULL, 'dqsddff@qsfqffdsqqsfq.fr', '$2y$12$Ypm0GWG8Vp3yrvOAVMrKzugOTswIrQ2pOw9YlbH5AO3hk9eeEA0qK', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(23, 0, 'test', 'terst', NULL, 'dsfsdfsdf@sdfsdfdsf.com', '$2y$12$u8tSCK7ZGL1.DxsuIfggAuJkSXH72WK74L/52LMZXkiHq0TkNv1Aq', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(24, 0, 'test', 'test', NULL, 'dfsgtsr@gtgg.fr', '$2y$12$uASe9IptvK/0Tnn4uo745OJ11d87IcrCpzcAhkHousmK2mC0yWYv6', NULL, NULL, NULL, NULL, NULL, NULL, 4),
+(25, 0, 'frffrsdf', 'sqfsqf', NULL, 'qff@fqfqfq.fr', '$2y$12$P052uY5G00whEBcxlDZ/zuq.rsAkMn.S6BFOYbUwknlKnPSgN99uK', NULL, NULL, NULL, NULL, NULL, NULL, 4);
 
 --
 -- Index pour les tables déchargées
@@ -190,7 +203,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Contraintes pour les tables déchargées
