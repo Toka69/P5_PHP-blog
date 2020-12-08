0f9d880c971de880c70ad4316f5d48ba09fbb201
Matthias <matthiasleroux@laposte.net>
2020-12-08 22:46:52 +0100

    add method contact to send an email

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,38 +12,55 @@ use Twig\Error\SyntaxError;
  * Class HomeController
  * @package App\Controller
  */
- class HomeController extends AbstractController
- {
-     /**
-      * @return Response
-      *
-      * @throws LoaderError
-      * @throws RuntimeError
-      * @throws SyntaxError
-      */
-       public function home(): Response
-       {
-            return $this->render("home.html.twig");
-       }
+class HomeController extends AbstractController
+{
+    /**
+    * @return Response
+    *
+    * @throws LoaderError
+    * @throws RuntimeError
+    * @throws SyntaxError
+    */
+    public function home(): Response
+    {
+        return $this->render("home.html.twig");
+    }
 
-     /**
-      * @return Response
-      */
-       public function urlError(): Response
-       {
-            return $this->redirect("notFound");
-       }
+    /**
+    * @return Response
+    */
+    public function urlError(): Response
+    {
+        return $this->redirect("notFound");
+    }
 
-     /**
-      * @return Response
-      *
-      * @throws LoaderError
-      * @throws RuntimeError
-      * @throws SyntaxError
-      */
-       public function notFound(): Response
+    /**
+    * @return Response
+    *
+    * @throws LoaderError
+    * @throws RuntimeError
+    * @throws SyntaxError
+    */
+    public function notFound(): Response
+    {
+        return $this->render("404.html.twig");
+    }
+
+    public function contact(){
+        if($_SERVER["REQUEST_METHOD"] === "POST")
         {
-            return $this->render("404.html.twig");
+         $secureRequestMethod = $this->secureRequestMethod($_POST);
+         $this->sendEmail($_ENV['MAIL_NOTIFICATION'], 'Nouvelle demande de contact depuis le site',
+
+'Demande de '.$secureRequestMethod['name'].'
+             
+'.$secureRequestMethod['message'].'
+
+'.$secureRequestMethod['email'].'            
+            
+            ');
         }
- }
- 
+
+        return $this->redirect("home");
+    }
+}
